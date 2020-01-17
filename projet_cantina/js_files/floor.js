ee6463f171b0d45ecca9fff830d01fcf713112ef
antoi <antoine.thiel@hotmail.Fr>
2020-01-17 10:16:19 +0100

    "fixed" seamless

--- projet_cantina/js_files/floor.js
+++ projet_cantina/js_files/floor.js
@@ -3,7 +3,11 @@ import * as THREE from './Dependencies/three.module.js';
 export function createFloor(){
 
      let sand = new THREE.TextureLoader().load('./textures/sand.jpg');
-    sand.repeat.set(20,20);  // not desired effect
+    // sand.repeat.set(20,20);  // not desired effect
+
+    sand.wrapS = THREE.RepeatWrapping;
+    sand.wrapT = THREE.RepeatWrapping;
+    sand.repeat.set(50,50);
     let floor_geo = new THREE.PlaneGeometry( 6000,6000,1,1);
 
     let floor_text = new THREE.MeshBasicMaterial({map:sand , side:THREE.DoubleSide});
